1c3ac5a6df5b8b94ab95190a4b4e6ef1d3d82122
MARK-PC\Mark <bigbluethe2@gmail.com>
2018-09-09 19:22:19 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String[] Items = {"Lettuce", "Tomatoes", "Ground Beef", "Bread", "Cheese", "Onions", "Pickles", "Potatoes"};
+
+        for(String Inventory: Items){
+            System.out.println(Inventory);
+        }
+    }
+}
